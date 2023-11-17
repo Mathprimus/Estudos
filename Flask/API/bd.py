52dd7e7a07e5481cef0c7d694e0bba0eb9ad4dcc
mathprimus <mathprimus@gmail.com>
2023-11-17 17:48:52 -0300

    API

---
+++ Flask/API/bd.py
@@ -0,0 +1,18 @@
+Carros = [
+{
+    'id': 1,
+    'marca': 'Fiat',
+},
+{
+    'id': 2,
+    'marca': 'Chevrolet',
+},
+{
+    'id': 3,
+    'marca': 'Volkswagen',
+},
+{
+    'id': 4,
+    'marca': 'Ferrari',
+}
+]
